b98d0597637c6018ba03b1180e0eb47b021c0cde
Paul Radzkov <radzkov@gmail.com>
2019-01-31 22:45:52 +0300

    added watch task

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
 # Scaffold
 
-Experiment with project file-structure and documentation.
+Experiment with project file-structure and documentation. With static sites in mind.
 
 ## What I want to build
 
 It is a template for a site with components documentation.
+
+## Gulp tasks
+
+`gulp clean` — delete `public` folder.  
+`gulp rendercss` — prepare css files.  
+`gulp watcher` — watch changes and recompile.
+
+`gulp build` — clean and build project.  
+`gulp dev` — clean, build and watch.  
+`gulp` — same as build.
--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,15 @@
-const { src, dest, parallel } = require('gulp');
-//const pug = require('gulp-pug');
-const less = require('gulp-less');
-const minifyCSS = require('gulp-csso');
-const rename = require('gulp-rename');
-//const concat = require('gulp-concat');
+const { src, dest, series, parallel, watch } = require('gulp');
 const del = require('delete');
+const less = require('gulp-less');
+const cssmin = require('gulp-csso');
+const rename = require('gulp-rename');
 const sourcemaps = require('gulp-sourcemaps');
+//const pug = require('gulp-pug');
+//const concat = require('gulp-concat');
 
 function clean(cb) {
-  // Use the `delete` module directly, instead of using gulp-rimraf
-  del(['public'], cb);
+    // Use the `delete` module directly, instead of using gulp-rimraf
+    del(['public'], cb);
 }
 
 //function html() {
@@ -23,9 +23,9 @@ function rendercss(cb) {
     return src('src/ui/**/*.less')
         .pipe(sourcemaps.init())
         .pipe(less())
-        .pipe(src('src/ui/**/*.css'))
+        .pipe(src(['src/ui/**/*.css']))
         .pipe(dest('public/ui'))
-        .pipe(minifyCSS())
+        .pipe(cssmin())
         .pipe(rename({ extname: '.min.css' }))
         .pipe(sourcemaps.write(''))
         .pipe(dest('public/ui'));
@@ -37,8 +37,15 @@ function rendercss(cb) {
 //    .pipe(dest('build/js', { sourcemaps: true }))
 //}
 
+function watcher() {
+    watch(['src/ui/**/*.less', 'src/ui/**/*.css'], rendercss);
+}
+
 exports.clean = clean;
 //exports.js = js;
 exports.rendercss = rendercss;
 //exports.html = html;
-exports.default = parallel(rendercss);
+exports.watcher = watcher;
+exports.build = series(clean, parallel(rendercss));
+exports.dev = series(clean, parallel(rendercss), watcher);
+exports.default = series(clean, parallel(rendercss));
